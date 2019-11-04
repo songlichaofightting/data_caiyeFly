c8047231b7e43a46c698abc5c123526e06cdd49a
songlichao <songlichao@kedabeijing.com>
2019-11-04 14:59:35 +0800

    代码骨架搭建

---
+++ restservies/src/main/java/group/mywork/restservies/RestserviesApplication.java
@@ -0,0 +1,13 @@
+package group.mywork.restservies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestserviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestserviesApplication.class, args);
+    }
+
+}
